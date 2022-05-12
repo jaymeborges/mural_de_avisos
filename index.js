d675e5ee2469e53506c9f41f84659eca3861f11e
Jayme Borges <jayme.moia@gmail.com>
2022-05-12 05:57:02 -0300

    update

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 const PORT = 3000
 const express = require('express')
-const bodyParser = require ('body-parser')
+const path = require("path")
+const apiRoutes = require('./routes/api')
 
 const app = express()
 
-let posts = [
-    {
-    id:"sdgghhh",
-    title:"Teste do Mural",
-    description:"Descricao Teste"
-    },
-]
-
-app.get("/all", (req,res)=>{
-
-    res.json(JSON.stringify(posts))
-
-})
-
-app.post("/new", bodyParser.json(), (req,res)=>{
-
-    let id = generateID()
-    let title = req.body.title
-    let description = req.body.description
-
-    posts.push({id,title,description})
-
-    res.send("Post adicionado")
-    
-})
+app.use('/api', apiRoutes)
+app.use('/', express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, ()=>{
      console.log("Server runing on port", PORT)
 })
 
-function generateID(){
-    return Math.random().toString(36).substr(2, 9)
-}
--- model/posts.js
+++ model/posts.js
@@ -8,5 +8,22 @@ module.exports = {
         },
     ],
     
+    getAll(){
+        return this.posts
+    },
 
+    newPost(title,description){
+
+        this.posts.push({id:generateID,title,description})
+
+    },
+
+    deletePost(id){
+        
+    }
+
+}
+
+function generateID(){
+    return Math.random().toString(36).substr(2, 9)
 }
